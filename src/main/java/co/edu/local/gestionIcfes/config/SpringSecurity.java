41b28502285f4dd33acd93b6121e1c5864d6bc04
Juan Manuel Villalba Rincon <juanvillalbarincon@gmail.com>
2026-05-07 11:40:08 -0500

    feat: Adding Security Configuration

---
+++ src/main/java/co/edu/local/gestionIcfes/config/SpringSecurity.java
@@ -0,0 +1,56 @@
+package co.edu.local.gestionIcfes.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import co.edu.local.gestionIcfes.services.UsuarioServices;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+	
+	@Autowired
+	private UsuarioServices usuarioServices;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider(usuarioServices);
+		auth.setPasswordEncoder(passwordEncoder);
+		return auth;
+	}
+	
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authenticationProvider(authenticationProvider()) 
+            .authorizeHttpRequests(auth -> auth
+            		.requestMatchers("/estilos/**", "/js/**", "/img/**", "/css/**").permitAll()
+            		.requestMatchers("/login").permitAll()
+                .requestMatchers("/pAdmin","/AdminEstudiante", "/pDocente", "/pEstudiante").hasRole("ADMIN")
+                .requestMatchers("/pDocente").hasAnyRole("DOCENTE")
+                .requestMatchers("/pEstudiante").hasAnyRole("ESTUDIANTE")
+                .anyRequest().authenticated()                                  
+            )
+            .formLogin(form -> form
+                .loginPage("/login")           
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+            ).exceptionHandling(exception -> exception.accessDeniedPage("/403"));
+
+        return http.build();
+    }
+}
