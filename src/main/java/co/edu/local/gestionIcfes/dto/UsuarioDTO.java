eb5c473fd7c96a191d85d576a4f50cc2d6292aa4
Miguel Medina Diaz <migue.a.medina06@gmail.com>
2026-05-07 11:10:24 -0500

    Feat: Adding UsuarioDTO

---
+++ src/main/java/co/edu/local/gestionIcfes/dto/UsuarioDTO.java
@@ -0,0 +1,45 @@
+package co.edu.local.gestionIcfes.dto;
+
+import java.util.Collection;
+
+import co.edu.local.gestionIcfes.model.Institucion;
+import co.edu.local.gestionIcfes.model.Rol;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+	private Long id;
+	
+	private String username;
+	
+	private String password;
+	
+	private Boolean enabled = true;
+	
+	private Institucion institucion;
+
+    private Rol roles;
+
+	public UsuarioDTO(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public UsuarioDTO(String username, String password, Institucion institucion, Rol roles) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.institucion = institucion;
+		this.roles = roles;
+	}
+	
+	
+    
+    
+}
