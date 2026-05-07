c62f4ddd8acff8edd84b7555e2aab6e8bcb931bf
Miguel Medina Diaz <migue.a.medina06@gmail.com>
2026-05-07 11:09:38 -0500

    Feat: Adding Services to User entity.
    Implements the feature to create Users.

---
+++ src/main/java/co/edu/local/gestionIcfes/services/UsuarioServices.java
@@ -0,0 +1,11 @@
+package co.edu.local.gestionIcfes.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import co.edu.local.gestionIcfes.dto.UsuarioDTO;
+import co.edu.local.gestionIcfes.model.Usuario;
+
+public interface UsuarioServices extends UserDetailsService{
+
+	public Usuario crearUsuario(UsuarioDTO usuarioDTO);
+}
