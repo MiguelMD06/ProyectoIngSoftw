1a03aa4a3c43e2723eca9256f3aa56abcf8a9cb0
Miguel Medina Diaz <migue.a.medina06@gmail.com>
2026-05-07 09:03:48 -0500

    First Commit: Subiendo Archivos Maven

---
+++ src/main/java/co/edu/local/gestionIcfes/GestionIcfesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.local.gestionIcfes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionIcfesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionIcfesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/local/gestionIcfes/GestionIcfesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.local.gestionIcfes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionIcfesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
