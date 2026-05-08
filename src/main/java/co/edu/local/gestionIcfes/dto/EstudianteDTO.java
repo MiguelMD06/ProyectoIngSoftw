9ab5ffbc9d39aaee44121b945526bc38505b6014
Miguel Medina Diaz <migue.a.medina06@gmail.com>
2026-05-08 11:44:44 -0500

    fix: Remove deprecated fields in Docente,Institucion and Estudiante DTOs

--- src/main/java/co/edu/local/gestionIcfes/dto/EstudianteDTO.java
+++ src/main/java/co/edu/local/gestionIcfes/dto/EstudianteDTO.java
@@ -7,7 +7,6 @@ import co.edu.local.gestionIcfes.model.Asistencia;
 import co.edu.local.gestionIcfes.model.Institucion;
 import co.edu.local.gestionIcfes.model.Persona;
 import co.edu.local.gestionIcfes.model.ResultadoSimulacro;
-import co.edu.local.gestionIcfes.model.Salon;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +17,6 @@ import lombok.NoArgsConstructor;
 public class EstudianteDTO extends Persona {
 	private Boolean activo = true;
     private Institucion institucion;
-    private Salon salon;
     private List<Asistencia> asistencias;
     private List<ResultadoSimulacro> resultados;
 
