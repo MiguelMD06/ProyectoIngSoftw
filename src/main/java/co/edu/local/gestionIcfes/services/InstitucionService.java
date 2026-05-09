ba51c6da8ce82859ee07cc2d5d1568bd89f8cb01
Juan Fierro <juandafica19@gmail.com>
2026-05-09 07:47:21 -0500

    chore(institution): implement institution service layer
    - Added InstitucionService interface with methods to list, save, and find institutions
    - Implemented InstitucionServiceImpl using InstitucionRepositorio
    - Enabled AdminController to dynamically fetch institutions and register new ones

--- src/main/java/co/edu/local/gestionIcfes/services/InstitucionService.java
+++ src/main/java/co/edu/local/gestionIcfes/services/InstitucionService.java
@@ -7,5 +7,6 @@ import co.edu.local.gestionIcfes.model.Institucion;
 public interface InstitucionService {
 	
 	public List<Institucion> listarInstituciones();
+    Institucion guardarInstitucion(Institucion institucion);
 	public Institucion buscarPorId(Long id);
 }
