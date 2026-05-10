a91c77953b1cc0d686c12e0c031d74751355f7d5
Miguel Medina Diaz <migue.a.medina06@gmail.com>
2026-05-10 17:05:21 -0500

    refactor: Deprecated unused Model, materialAcademico

--- src/main/java/co/edu/local/gestionIcfes/repository/MaterialAcademicoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package co.edu.local.gestionIcfes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.local.gestionIcfes.model.MaterialAcademico;
-
-public interface MaterialAcademicoRepositorio extends JpaRepository<MaterialAcademico, Long> {
-
-}
