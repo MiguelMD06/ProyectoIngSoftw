cd2ebae53c6ecc4f6a6273ec1e58b55e57c8d99f
Juan Fierro <juandafica19@gmail.com>
2026-05-10 15:11:57 -0500

    feat(repository): add method to find simulacro by title

--- src/main/java/co/edu/local/gestionIcfes/repository/SimulacroRepositorio.java
+++ src/main/java/co/edu/local/gestionIcfes/repository/SimulacroRepositorio.java
@@ -1,9 +1,12 @@
 package co.edu.local.gestionIcfes.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.edu.local.gestionIcfes.model.Simulacro;
 
 public interface SimulacroRepositorio extends JpaRepository<Simulacro, Long> {
+	Optional<Simulacro> findByTitulo(String titulo);
 
 }
